2630ad07a48d777ca27308eaa6ce4211c1d82636
Sergey Bendas <seregabendas@gmail.com>
2017-11-09 14:06:49 +0200

    Homework #5

---
+++ task_0/sergey.bendas/src/com/hillel/Task.java
@@ -0,0 +1,24 @@
+package com.hillel;
+
+import java.util.Scanner;
+
+public class Task{
+  
+  public static void main(String... args){
+    
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.print("Enter length: ");
+    
+    int arrayLength = scan.nextInt();
+    
+    int[] array = new int[arrayLength];
+    
+    for(int i = 0; i< array.length; i++){
+      System.out.print("Enter element #" + i + " : ");
+      array[i] = scan.nextInt();
+    }
+    
+    
+  }
+}
