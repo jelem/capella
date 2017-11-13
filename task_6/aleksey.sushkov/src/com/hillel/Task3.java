325abdf1ed0bfa38e3c11863c6473419abef2622
Aleksey Sushkov <aleksey.syshkov@gmail.com>
2017-11-13 12:00:48 +0200

    Homework for Lesson 6

---
+++ task_6/aleksey.sushkov/src/com/hillel/Task3.java
@@ -0,0 +1,19 @@
+package com.hillel;
+
+import java.util.Scanner;
+
+public class Task3 {
+	
+	public static void main(String... args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the size of your array: ");
+		int n = scanner.nextInt();
+		int arr[] = new int[n];
+		System.out.print("Your array: ");
+		for (int i = 0, j = n; i < n && j > 0; i++, j--) {
+			arr[i] = j;
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
