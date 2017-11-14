1b3bf65830071bb76b09e3759b6f4bc5e851f84d
darknsoft <samsonov.omr@gmail.com>
2017-11-14 20:46:46 +0200

    Style 01

--- task_6/andrew.samsonov/src/main/java/Task1.java
+++ task_6/andrew.samsonov/src/main/java/Task1.java
@@ -7,13 +7,13 @@ public class Task1 {
     int[] array = new int[10];
     init(array);
 
-    for ( int i = 0; i < 5; i++) {
+    for ( int i = 0; i < 5; i++ ) {
       System.out.print(i + " ");
     }
   }
 
   private static int[] init(int[] ar){
-    for (int i=0; i < ar.length; i++) {
+    for ( int i=0; i < ar.length; i++ ) {
       ar[i]=i;
     }
     return ar;
