1072ea971141eb7823d4ba6015c8d71c5e6a928d
darknsoft <samsonov.omr@gmail.com>
2017-11-14 20:58:28 +0200

    Task_2

--- task_6/andrew.samsonov/src/main/java/Task2.java
+++ task_6/andrew.samsonov/src/main/java/Task2.java
@@ -1,21 +1,23 @@
 /**
  * Created by Andrew on 14.11.2017.
  */
-public class Task1 {
+public class Task2 {
 
   public static void main(String[] args) {
-    int[] array = new int[10];
+    int n = 25;
+    int[] array = new int[n];
     init(array);
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < array.length; i++) {
       System.out.print(array[i] + " ");
     }
   }
 
   private static int[] init(int[] ar) {
     for (int i = 0; i < ar.length; i++) {
-      ar[i] = i;
+      ar[i] = i+1;
     }
     return ar;
   }
+
 }
