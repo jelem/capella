eef3b2fed132664e6e9cb21d42573cbfed039686
Andrew <samsonov.a@ukr.net>
2017-11-14 20:10:31 +0200

    Task6_1

---
+++ task_6/andrew.samsonov/src/main/java/Task1.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Andrew on 14.11.2017.
+ */
+public class Task1 {
+
+  public static void main(String[] args) {
+    int[] array = new int[10];
+    init(array);
+
+    for ( int i = 0; i < 5; i++) {
+      System.out.print(i + " ");
+    }
+  }
+
+  private static int[] init(int[] ar){
+    for (int i=0; i < ar.length; i++) {
+      ar[i]=i;
+    }
+    return ar;
+  }
+}
