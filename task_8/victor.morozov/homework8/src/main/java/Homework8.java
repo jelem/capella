761a88f66449fc850f2441483d7b5cc0c13304ce
Victor Morozov <morozovvic@gmail.com>
2017-11-22 00:03:29 +0200

    Homework8. Fixsed main.

--- task_8/victor.morozov/homework8/src/main/java/Homework8.java
+++ task_8/victor.morozov/homework8/src/main/java/Homework8.java
@@ -7,16 +7,38 @@ import java.util.Scanner;
 public class Homework8 {
 
   public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      System.out.print("Input N: ");
-      int n = scanner.nextInt();
-/*      int[][] pascal = arrayInput(n);
-      arrayPrint(pascal);*/
+    boolean exit = false;
+    int number = 0;
+    Scanner scanner = new Scanner(System.in);
 
-      int[][] array = SpiralArray.arrayInit(n);
-      array = SpiralArray.fillArraySpiral(array, n);
-      SpiralArray.arrayPrint(array);
+    do {
+      System.out.println();
+      System.out.println("1. Pascal Triangle");
+      System.out.println("2. Spiral Array");
+      System.out.println("3. Exit");
 
+      System.out.print("Input variant: ");
+      int variant = scanner.nextInt();
+
+      switch (variant) {
+        case 1:
+          System.out.print("Pascal Triangle: Input number = ");
+          number = scanner.nextInt();
+          System.out.println();
+          int[][] pascal = arrayInput(number);
+          arrayPrint(pascal);
+          break;
+        case 2:
+          System.out.print("Spiral Array: Input number = ");
+          number = scanner.nextInt();
+          System.out.println();
+          int[][] array = SpiralArray.arrayInit(number);
+          array = SpiralArray.fillArraySpiral(array, number);
+          SpiralArray.arrayPrint(array);
+          break;
+        default:
+          exit = true;
+      }
+    } while (!exit);
   }
-
 }
