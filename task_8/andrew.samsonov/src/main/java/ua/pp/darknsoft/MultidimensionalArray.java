c3f16734b3c08e65a708a1eb4e53653720861aa1
darknsoft <samsonov.omr@gmail.com>
2017-11-21 17:08:57 +0200

    Helix method

--- task_8/andrew.samsonov/src/main/java/ua/pp/darknsoft/MultidimensionalArray.java
+++ task_8/andrew.samsonov/src/main/java/ua/pp/darknsoft/MultidimensionalArray.java
@@ -1,13 +1,8 @@
 package ua.pp.darknsoft;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
-
-/**
- * Created by Andrew on 20.11.2017.
- */
 public class MultidimensionalArray {
 
-  public static int[][] pasckalTriangle(int length) {
+  public static int[][] pascalTriangle(int length) {
     int[][] autoPascal = new int[length][];
 
     for (int i = 0; i < autoPascal.length; i++) {
@@ -29,9 +24,51 @@ public class MultidimensionalArray {
     return autoPascal;
   }
 
-  public static int[][] spiral(int length) {
-    int[][] helix = {{1, 2, 3, 4}, {12, 13, 14, 5}, {11, 16, 15, 6},
-        {10, 9, 8, 7}};
+  public static int[][] helixInit(int length) {
+    if (length <= 0) {
+      return new int[0][0];
+    }
+    int[][] helix = new int[length][length];
+    int x1 = 0;
+    int y1 = length - 1;
+    int x2 = length - 1;
+    int y2 = 0;
+    int tmp = 1;
+    int steps = 0;
+    if (length % 2 == 0) {
+      steps = length / 2;
+    } else {
+      steps = (length / 2) + 1;
+    }
+    for (int k = 0; k < steps; k++) {
+
+      for (int i = x1; i <= y1; i++) {
+        for (int j = y2; j <= y1; j++) {
+
+          if (i != x1 && j != y1) {
+            helix[i][j] = 0;
+          } else {
+            helix[i][j] = tmp;
+            tmp++;
+          }
+        }
+      }
+      y1--;
+      x1++;
+      for (int i = x2; i >= x1; i--) {
+        for (int j = y1; j >= y2; j--) {
+          if (i != x2 && j != y2) {
+            helix[i][j] = 0;
+          } else {
+            helix[i][j] = tmp;
+            tmp++;
+
+          }
+        }
+      }
+      x2--;
+      y2++;
+    }
 
     return helix;
   }
