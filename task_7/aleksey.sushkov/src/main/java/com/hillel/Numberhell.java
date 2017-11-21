5068ea832651d1d7db88423338a856342447c4dc
Allekseys <33160295+Allekseys@users.noreply.github.com>
2017-11-21 22:31:02 +0200

    Upgraded

--- task_7/aleksey.sushkov/src/main/java/com/hillel/Numberhell.java
+++ task_7/aleksey.sushkov/src/main/java/com/hillel/Numberhell.java
@@ -4,12 +4,14 @@ public class Numberhell {
 
   public static int sumOfNumbers(int numb) {
 
-    int lastnumb = 0, res = 0;
+    int lastnumb = 0;
+    int res = 0;
     do {
       lastnumb = numb % 10;
       res += lastnumb;
       numb /= 10;
-    } while (numb > 0);
+    } 
+    while (numb > 0);
     return res;
   }
 
@@ -17,8 +19,7 @@ public class Numberhell {
     int res = 1;
     if (numb == 0) {
       return 1;
-    }
-    else {
+    } else {
       for (int i = 1; i <= numb; i++) {
         res *= i;
       }
@@ -30,7 +31,7 @@ public class Numberhell {
   public static int triangleCalculation(int side, String formula) {
 
     int res = 0;
-    switch(formula) {
+    switch (formula) {
       case "perimeter":
         res = side * 4;
         break;
