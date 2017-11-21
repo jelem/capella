126aa8298195cca5d1e800c76675d04ddc27e557
Allekseys <aleksey.syshkov@gmail.com>
2017-11-21 18:56:44 +0200

    Homework_7

---
+++ task_7/aleksey.sushkov/src/test/java/com/hillel/NumberhellTest.java
@@ -0,0 +1,62 @@
+package com.hillel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NumberhellTest {
+
+  @Test
+  public void shouldSumOfNumbers1() {
+    int result = Numberhell.sumOfNumbers(123453);
+    assertEquals(18, result);
+  }
+
+  @Test
+  public void shouldSumOfNumbers2() {
+    int result = Numberhell.sumOfNumbers(5683084);
+    assertEquals(34, result);
+  }
+
+  @Test
+  public void shouldSumOfNumbers3() {
+    int result = Numberhell.sumOfNumbers(2);
+    assertEquals(2, result);
+  }
+
+  @Test
+  public void shouldfactorial1() {
+    int result = Numberhell.factorial(5);
+    assertEquals(120, result);
+  }
+
+  @Test
+  public void shouldfactorial2() {
+    int result = Numberhell.factorial(12);
+    assertEquals(479001600, result);
+  }
+
+  @Test
+  public void shouldfactorial3() {
+    int result = Numberhell.factorial(0);
+    assertEquals(1, result);
+  }
+
+  @Test
+  public void shouldtriangle1() {
+    int result = Numberhell.triangleCalculation(5,"perimeter");
+    assertEquals(20, result);
+  }
+
+  @Test
+  public void shouldtriangle2() {
+    int result = Numberhell.triangleCalculation(26,"square");
+    assertEquals(676, result);
+  }
+
+  @Test
+  public void shouldtriangle3() {
+    int result = Numberhell.triangleCalculation(11,"wrong_parameter");
+    assertEquals(0, result);
+  }
+}
