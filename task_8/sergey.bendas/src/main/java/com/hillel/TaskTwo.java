11db931f9929bc4ffc70355464377a68092b81ae
Sergey Bendas <seregabendas@gmail.com>
2017-11-22 16:32:05 +0200

    i/jCoordinate -> len/widthCoord

--- task_8/sergey.bendas/src/main/java/com/hillel/TaskTwo.java
+++ task_8/sergey.bendas/src/main/java/com/hillel/TaskTwo.java
@@ -14,38 +14,38 @@ public class TaskTwo {
     return spiralArray;
   }
 
-  static int findElementAt(int iCoordinate, int jCoordinate, int len, int width) {
+  static int findElementAt(int lenCoord, int widthCoord, int len, int width) {
 
     return allElements(len, width)
-        - insideArraySquare(iCoordinate, jCoordinate, len, width)
-        - remainOfRound(iCoordinate, jCoordinate, len, width);
+        - insideArraySquare(lenCoord, widthCoord, len, width)
+        - remainOfRound(lenCoord, widthCoord, len, width);
   }
 
-  static int remainOfRound(int iCoordinate, int jCoordinate, int len, int width) {
+  static int remainOfRound(int lenCoord, int widthCoord, int len, int width) {
 
-    int border = findBorder(iCoordinate, jCoordinate, len, width);
+    int border = findBorder(lenCoord, widthCoord, len, width);
     int right = 0;
     int top = 0;
     int left;
     int bottom = 0;
-    switch (checkSide(iCoordinate, jCoordinate, len, width)) {
+    switch (checkSide(lenCoord, widthCoord, len, width)) {
       case "top":
-        top = width - jCoordinate - 1 - (border - 1);
+        top = width - widthCoord - 1 - (border - 1);
         right = len - 1 - 2 * (border - 1);
         bottom = width - 1 - 2 * (border - 1);
         left = len - 2 - 2 * (border - 1);
         break;
       case "right":
-        right = len - iCoordinate - 1 - (border - 1);
+        right = len - lenCoord - 1 - (border - 1);
         bottom = width - 1 - 2 * (border - 1);
         left = len - 2 - 2 * (border - 1);
         break;
       case "bottom":
-        bottom = jCoordinate + 1 - border;
+        bottom = widthCoord + 1 - border;
         left = len - 2 - 2 * (border - 1);
         break;
       case "left":
-        left = iCoordinate - border;
+        left = lenCoord - border;
         break;
       default:
         return 0;
@@ -53,27 +53,27 @@ public class TaskTwo {
     return left + top + right + bottom;
   }
 
-  private static String checkSide(int iCoordinate, int jCoordinate, int len, int width) {
-    int border = findBorder(iCoordinate, jCoordinate, len, width);
-    if (iCoordinate < border) {
+  private static String checkSide(int lenCoord, int widthCoord, int len, int width) {
+    int border = findBorder(lenCoord, widthCoord, len, width);
+    if (lenCoord < border) {
       return "top";
-    } else if (jCoordinate >= width - border) {
+    } else if (widthCoord >= width - border) {
       return "right";
-    } else if (iCoordinate >= len - border) {
+    } else if (lenCoord >= len - border) {
       return "bottom";
     } else {
       return "left";
     }
   }
 
-  static int insideArraySquare(int iCoordinate, int jCoordinate, int len, int width) {
-    int border = findBorder(iCoordinate, jCoordinate, len, width);
+  static int insideArraySquare(int lenCoord, int widthCoord, int len, int width) {
+    int border = findBorder(lenCoord, widthCoord, len, width);
     return (len - 2 * border) * (width - 2 * border);
   }
 
-  static int findBorder(int iCoordinate, int jCoordinate, int len, int width) {
-    int borderX = iCoordinate < len / 2 ? iCoordinate + 1 : len - iCoordinate;
-    int borderY = jCoordinate < width / 2 ? jCoordinate + 1 : width - jCoordinate;
+  static int findBorder(int lenCoord, int widthCoord, int len, int width) {
+    int borderX = lenCoord < len / 2 ? lenCoord + 1 : len - lenCoord;
+    int borderY = widthCoord < width / 2 ? widthCoord + 1 : width - widthCoord;
     return borderX < borderY ? borderX : borderY;
   }
 
