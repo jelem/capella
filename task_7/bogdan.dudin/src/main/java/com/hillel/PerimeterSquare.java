77bdb1793d06f3b931dac89421be13129423cb4d
Bogdan010 <bohdan010@gmail.com>
2017-11-27 18:39:46 +0200

    Update PerimeterSquare.java

--- task_7/bogdan.dudin/src/main/java/com/hillel/PerimeterSquare.java
+++ task_7/bogdan.dudin/src/main/java/com/hillel/PerimeterSquare.java
@@ -3,9 +3,9 @@ package com.hillel;
 public class PerimeterSquare {
 
     public static int calculation(int side, String formula) {
-        if (formula.eqals("perimeter"))
+        if (formula.equals("perimeter"))
             return side * 4;
-        else if (formula.eqals("square"))
+        else if (formula.equals("square"))
             return side * side;
         else
             return 0;
