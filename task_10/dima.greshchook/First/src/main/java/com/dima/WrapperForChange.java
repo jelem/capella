607cd69e8bfe969edc3935d4311234158210f1cb
dima <greshchook@gmail.com>
2017-11-28 19:26:31 +0200

    I've fix this

--- task_10/dima.greshchook/First/src/main/java/com/dima/WrapperForChange.java
+++ task_10/dima.greshchook/First/src/main/java/com/dima/WrapperForChange.java
@@ -7,18 +7,7 @@ public class WrapperForChange {
     if (!isStringIncludeChar(old, '.') && !isStringIncludeChar(old, '@')){
       return old;
     }
-    String newString = "";
-    for (int i = 0; i < old.length(); i++) {
-      char ch = old.charAt(i);
-      if ( ch != '@' && ch != '.') {
-        newString += ch;
-      }else if (ch == '@'){
-        newString += "[at]";
-      }else  {
-        newString += "[dot].";
-      }
-    }
-    return  newString;
+    return old.replace("@","[at]").replace(".","[dot].");
   }
 
   public static boolean isStringIncludeChar(String old, char ch) {
