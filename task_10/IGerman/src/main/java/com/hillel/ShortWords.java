d70896e7ec74855e1bda6b60062552c4146a0026
IrinaGerman <iringerman30@gmail.com>
2017-11-29 21:23:44 +0200

    Without bugs

--- task_10/IGerman/src/main/java/com/hillel/ShortWords.java
+++ task_10/IGerman/src/main/java/com/hillel/ShortWords.java
@@ -3,11 +3,12 @@ package com.hillel;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class ShortWords {
 
   public static void main(String[] args) throws IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
     String str = reader.readLine();
     reader.close();
     makingShortWords(str);
