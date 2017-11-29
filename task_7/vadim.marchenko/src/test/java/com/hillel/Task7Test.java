d69825ae58b7ac69f9e8987f0ecf44b1afad5419
Vadim.Marchenko <wardomir@gmail.com>
2017-11-29 21:03:36 +0200

    corrected indentation

--- task_7/vadim.marchenko/src/test/java/com/hillel/Task7Test.java
+++ task_7/vadim.marchenko/src/test/java/com/hillel/Task7Test.java
@@ -6,26 +6,26 @@ import static org.junit.Assert.*;
 
 public class Task7Test {
 
-    @Test
-    public void shouldAddDigitsTest() {
-        assertEquals(Task7.addDigits(421), 7);
-        assertEquals(Task7.addDigits(999999), 54);
-        assertEquals(Task7.addDigits(1), 1);
-        assertEquals(Task7.addDigits(0), 0);
-    }
+  @Test
+  public void shouldAddDigitsTest() {
+    assertEquals(Task7.addDigits(421), 7);
+    assertEquals(Task7.addDigits(999999), 54);
+    assertEquals(Task7.addDigits(1), 1);
+    assertEquals(Task7.addDigits(0), 0);
+  }
 
-    @Test
-    public void shouldCalculateFactorialTest() {
-        assertEquals(Task7.numberFactorial(5), 120);
-        assertEquals(Task7.numberFactorial(6), 720);
-        assertEquals(Task7.numberFactorial(7), 5040);
-        assertEquals(Task7.numberFactorial(1), 1);
-    }
+  @Test
+  public void shouldCalculateFactorialTest() {
+    assertEquals(Task7.numberFactorial(5), 120);
+    assertEquals(Task7.numberFactorial(6), 720);
+    assertEquals(Task7.numberFactorial(7), 5040);
+    assertEquals(Task7.numberFactorial(1), 1);
+  }
 
-    @Test
-    public void shouldCalculateSquareFormulas() {
-        assertEquals(Task7.triangleCalculation(5, "perimeter"), 20);
-        assertEquals(Task7.triangleCalculation(5, "square"), 25);
-        assertEquals(Task7.triangleCalculation(5, "SomethingElse"), 0);
-    }
+  @Test
+  public void shouldCalculateSquareFormulas() {
+    assertEquals(Task7.triangleCalculation(5, "perimeter"), 20);
+    assertEquals(Task7.triangleCalculation(5, "square"), 25);
+    assertEquals(Task7.triangleCalculation(5, "SomethingElse"), 0);
+  }
 }
