182794dd47594dfdffe2037f2b701979ac721ce0
Victor Morozov <morozovvic@gmail.com>
2017-12-05 22:41:20 +0200

    Homework#12. n -> number or num

--- task_11/victor.morozov/src/main/java/com/hillel/MemberNOfSequenceIterative.java
+++ task_11/victor.morozov/src/main/java/com/hillel/MemberNOfSequenceIterative.java
@@ -2,15 +2,15 @@ package com.hillel;
 
 public class MemberNOfSequenceIterative {
 
-  public static int memberNOfSequenceIterative(int n) {
+  public static int memberNOfSequenceIterative(int number) {
     int m1 = 1;
     int m2 = 1;
     int m3 = 1;
     int mn = 0;
-    if (n < 4) {
+    if (number < 4) {
       return 1;
     }
-    for (int i = 4; i <= n ; i++) {
+    for (int i = 4; i <= number ; i++) {
       mn = m3 + m2 + m1;
       m1 = m2;
       m2 = m3;
--- task_11/victor.morozov/src/main/java/com/hillel/MemberNOfSequenceRecursive.java
+++ task_11/victor.morozov/src/main/java/com/hillel/MemberNOfSequenceRecursive.java
@@ -2,11 +2,11 @@ package com.hillel;
 
 public class MemberNOfSequenceRecursive {
 
-  public static int memberNOfSequence(int n) {
-    if (n == 3 || n == 2 || n == 1) {
+  public static int memberNOfSequence(int num) {
+    if (num == 3 || num == 2 || num == 1) {
       return 1;
     }
-    return memberNOfSequence(n - 3) + memberNOfSequence(n - 2) + memberNOfSequence(n - 1);
+    return memberNOfSequence(num - 3) + memberNOfSequence(num - 2) + memberNOfSequence(num - 1);
   }
 
 }
