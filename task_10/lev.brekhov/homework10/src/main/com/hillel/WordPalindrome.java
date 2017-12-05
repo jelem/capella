ce87af497171c62f2770bbaf49dd8cd388e8d91b
Lev Tesla <levonik99@gmail.com>
2017-12-05 13:00:26 +0200

    Homework 11

--- task_10/lev.brekhov/homework10/src/main/com/hillel/WordPalindrome.java
+++ task_10/lev.brekhov/homework10/src/main/com/hillel/WordPalindrome.java
@@ -13,16 +13,22 @@ public class WordPalindrome {
 
     for (int i = 0; i < arrayStrings.length; i++) {
       arrayStrings[i] = arrayStrings[i].trim();
-      isPalindrome = true;
-      for (int j = 0; j < arrayStrings[i].length() / 2; j++) {
-        if (arrayStrings[i].charAt(j) != arrayStrings[i].charAt(arrayStrings[i].length() - 1 - j)) {
-          isPalindrome = false;
-        }
-      }
+      isPalindrome = isWordPalimdrome(arrayStrings[i]);
       if (isPalindrome) {
         count++;
       }
     }
     System.out.println("We have " + count + " palindromes!!");
+  }
+
+  public static boolean isWordPalimdrome(String word) {
+
+    boolean isPalindrome = true;
+    for (int j = 0; j < word.length() / 2; j++) {
+      if (word.charAt(j) != word.charAt(word.length() - 1 - j)) {
+        isPalindrome = false;
+      }
+    }
+    return isPalindrome;
   }
 }
