19a2792242dc7a890112720c4f979404d9021c80
Vadim.Marchenko <wardomir@gmail.com>
2017-12-07 17:03:14 +0200

    Task 10 Test update

--- task_10/vadim.marchenko/src/test/java/com/hillel/WordShorteningTest.java
+++ task_10/vadim.marchenko/src/test/java/com/hillel/WordShorteningTest.java
@@ -10,23 +10,22 @@ public class WordShorteningTest {
   public void reductionTest() {
     String word1 = "internationalization";
     String word1Expected = "i18n";
+    assertEquals(WordShortening.reduction(word1), word1Expected);
 
     String word2 = "localization";
     String word2Expected = "l10n";
+    assertEquals(WordShortening.reduction(word2), word2Expected);
 
     String word3 = "cat";
     String word3Expected = "cat";
+    assertEquals(WordShortening.reduction(word3), word3Expected);
 
     String word4 = "elephant";
     String word4Expected = "e6t";
+    assertEquals(WordShortening.reduction(word4), word4Expected);
 
     String word5 = "monitor";
     String word5Expected = "m5r";
-
-    assertEquals(WordShortening.reduction(word1), word1Expected);
-    assertEquals(WordShortening.reduction(word2), word2Expected);
-    assertEquals(WordShortening.reduction(word3), word3Expected);
-    assertEquals(WordShortening.reduction(word4), word4Expected);
     assertEquals(WordShortening.reduction(word5), word5Expected);
   }
 
