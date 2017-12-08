84e3e0debd96d1a5c2829846910a231dfeb7526f
darknsoft <samsonov.omr@gmail.com>
2017-12-08 22:14:40 +0200

    Home work 13 - Standart Style 04

--- task_13/andrew.samsonov/src/test/java/ua/pp/darknsoft/TestComplexNumber.java
+++ task_13/andrew.samsonov/src/test/java/ua/pp/darknsoft/TestComplexNumber.java
@@ -24,70 +24,70 @@ public class TestComplexNumber {
 
   @Test
   public void additionTest01() {
-    ComplexNumber alfaComplex = new ComplexNumber(5, -6);
+    ComplexNumber alphaComplex = new ComplexNumber(5, -6);
     ComplexNumber betaComplex = new ComplexNumber(-3, 2);
-    ComplexNumber actual = alfaComplex.addition(betaComplex);
+    ComplexNumber actual = alphaComplex.addition(betaComplex);
     assertThat(actual, is(COMPLEX_NUMBER_ADDITION));
   }
 
   @Test
   public void subtractionTest01() {
-    ComplexNumber alfaComplex = new ComplexNumber(5, -6);
+    ComplexNumber alphaComplex = new ComplexNumber(5, -6);
     ComplexNumber betaComplex = new ComplexNumber(-3, 2);
-    ComplexNumber actual = alfaComplex.subtraction(betaComplex);
+    ComplexNumber actual = alphaComplex.subtraction(betaComplex);
     assertThat(actual, is(COMPLEX_NUMBER_SUBTRACTION));
   }
 
   @Test
   public void multiplicationTest01() {
-    ComplexNumber alfaComplex = new ComplexNumber(2, 3);
+    ComplexNumber alphaComplex = new ComplexNumber(2, 3);
     ComplexNumber betaComplex = new ComplexNumber(-1, 1);
-    ComplexNumber actual = alfaComplex.multiplication(betaComplex);
+    ComplexNumber actual = alphaComplex.multiplication(betaComplex);
     assertThat(actual, is(COMPLEX_NUMBER_MULTIPLICATION));
   }
 
   @Test
   public void multiplicationTest02() {
-    ComplexNumber alfaComplex = new ComplexNumber(2, 3);
+    ComplexNumber alphaComplex = new ComplexNumber(2, 3);
     ComplexNumber betaComplex = new ComplexNumber(0, 0);
-    ComplexNumber actual = alfaComplex.multiplication(betaComplex);
+    ComplexNumber actual = alphaComplex.multiplication(betaComplex);
     assertThat(actual, is(COMPLEX_NUMBER_ZERO));
   }
 
   @Test
   public void divisionTest01() {
-    ComplexNumber alfaComplex = new ComplexNumber(-2, 1);
+    ComplexNumber alphaComplex = new ComplexNumber(-2, 1);
     ComplexNumber betaComplex = new ComplexNumber(1, -1);
-    ComplexNumber actual = alfaComplex.division(betaComplex);
+    ComplexNumber actual = alphaComplex.division(betaComplex);
     assertThat(actual, is(COMPLEX_NUMBER_DIVISION));
   }
 
   @Test
   public void divisionTest02() {
-    ComplexNumber alfaComplex = new ComplexNumber(-2, 1);
+    ComplexNumber alphaComplex = new ComplexNumber(-2, 1);
     ComplexNumber betaComplex = new ComplexNumber(0, 0);
-    ComplexNumber actual = alfaComplex.division(betaComplex);
+    ComplexNumber actual = alphaComplex.division(betaComplex);
     assertThat(actual, is(COMPLEX_NUMBER_ZERO));
   }
 
   @Test
   public void absComplexNumberTest01() {
-    ComplexNumber alfaComplex = new ComplexNumber(5, -3);
-    double actual = alfaComplex.absComplexNumber(alfaComplex);
+    ComplexNumber alphaComplex = new ComplexNumber(5, -3);
+    double actual = alphaComplex.absComplexNumber(alphaComplex);
     assertThat(actual, is(ABSOLUTE_COMPLEX_NUMBER));
   }
 
   @Test
   public void argComplexNumberTest01() {
-    ComplexNumber alfaComplex = new ComplexNumber(-3, -3);
-    double actual = alfaComplex.argComplexNumber(alfaComplex);
+    ComplexNumber alphaComplex = new ComplexNumber(-3, -3);
+    double actual = alphaComplex.argComplexNumber(alphaComplex);
     assertThat(actual, is(ARGUMENT_OF_A_COMPLEX_NUMBER));
   }
 
   @Test
   public void extractionOfARootTest01() {
-    ComplexNumber alfaComplex = new ComplexNumber(-1, 0);
-    ComplexNumber[] actual = alfaComplex.extractionOfARoot(4);
+    ComplexNumber alphaComplex = new ComplexNumber(-1, 0);
+    ComplexNumber[] actual = alphaComplex.extractionOfARoot(4);
     assertThat(actual, is(COMPLEX_NUMBERS_SQRT_04));
   }
 }
