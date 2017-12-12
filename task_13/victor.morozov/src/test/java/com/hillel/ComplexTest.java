018e8e5df53855cfbce1bf9b6ad77c2acfc8a7b2
Victor Morozov <morozovvic@gmail.com>
2017-12-12 22:28:35 +0200

    Refactored project and renamed class

--- task_13/victor.morozov/src/test/java/com/hillel/ComplexTest.java
+++ task_13/victor.morozov/src/test/java/com/hillel/ComplexTest.java
@@ -9,17 +9,17 @@ public class ComplexTest {
 
   @Test
   public void addTest() {
-    Copmlex arg1 = new Copmlex(3, -2);
-    Copmlex arg2 = new Copmlex(2, 4);
+    Complex arg1 = new Complex(3, -2);
+    Complex arg2 = new Complex(2, 4);
     String expected = "5.0+2.0i";
-    arg1 = arg1.add(arg2);
-    assertThat(arg1.toString(), is(expected));
+    Complex arg3 = arg1.add(arg2);
+    assertThat(arg3.toString(), is(expected));
   }
 
   @Test
   public void subTest() {
-    Copmlex arg1 = new Copmlex(3, -2);
-    Copmlex arg2 = new Copmlex(2, 4);
+    Complex arg1 = new Complex(3, -2);
+    Complex arg2 = new Complex(2, 4);
     String expected = "1.0-6.0i";
     arg1 = arg1.sub(arg2);
     assertThat(arg1.toString(), is(expected));
@@ -27,8 +27,8 @@ public class ComplexTest {
 
   @Test
   public void multTest() {
-    Copmlex arg1 = new Copmlex(3, -2);
-    Copmlex arg2 = new Copmlex(2, 4);
+    Complex arg1 = new Complex(3, -2);
+    Complex arg2 = new Complex(2, 4);
     String expected = "14.0+8.0i";
     arg1 = arg1.mult(arg2);
     assertThat(arg1.toString(), is(expected));
@@ -36,8 +36,8 @@ public class ComplexTest {
 
   @Test
   public void divTest() {
-    Copmlex arg1 = new Copmlex(3, -2);
-    Copmlex arg2 = new Copmlex(2, 4);
+    Complex arg1 = new Complex(3, -2);
+    Complex arg2 = new Complex(2, 4);
     String expected = "-0.1+0.4i";
     arg1 = arg1.div(arg2);
     assertThat(arg1.toString(), is(expected));
@@ -45,7 +45,7 @@ public class ComplexTest {
 
   @Test
   public void sqrtPlusRootTest() {
-    Copmlex arg1 = new Copmlex(3, 4);
+    Complex arg1 = new Complex(3, 4);
     String expected = "2.0+1.0i";
     arg1 = arg1.sqrtPlusRoot();
     assertThat(arg1.toString(), is(expected));
@@ -53,7 +53,7 @@ public class ComplexTest {
 
   @Test
   public void sqrtMinusRootTest() {
-    Copmlex arg1 = new Copmlex(3, 4);
+    Complex arg1 = new Complex(3, 4);
     String expected = "-2.0-1.0i";
     arg1 = arg1.sqrtMinusRoot();
     assertThat(arg1.toString(), is(expected));
