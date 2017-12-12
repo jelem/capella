8c8b99c380242f65b165e37a278ead6f0cbe25e0
Lev Tesla <levonik99@gmail.com>
2017-12-12 23:47:44 +0200

    Homework 13 travisGone OK

--- task_14/lev.brekhov/src/main/java/com/lev/TicTacToe.java
+++ task_14/lev.brekhov/src/main/java/com/lev/TicTacToe.java
@@ -2,27 +2,38 @@ package com.lev;
 
 public class TicTacToe {
 
-  public static void main(String[] args) {
-
-    char[][] field = new char[3][3];
-
-    char currentPlayer = 'X';
-    fillBoard(field);
-
-    while (!gameFinished()) {
-      String coords;
-      do {
-        coords = inputCoordinates();
-      } while (!validateCoordinates(coords));
-      makeMove(field, coords, currentPlayer);
-      currentPlayer = switchCurrentPlayer(currentPlayer);
-    }
-
-    calculateWinner();
-  }
-
-  public static void fillBoard(char[][] field) {
-  }
+//  public static void main(String[] args) {
+//
+//    char[][] field = new char[3][3];
+//
+//    char currentPlayer = 'X';
+//    fillBoard(field);
+//
+//    while (!gameFinished()) {
+//      String coords;
+//      do {
+//        coords = inputCoordinates();
+//      } while (!validateCoordinates(coords));
+//      makeMove(field, coords, currentPlayer);
+//      currentPlayer = switchCurrentPlayer(currentPlayer);
+//    }
+//
+//    calculateWinner();
+//  }
+//
+//  private static String inputCoordinates() {
+//
+//    return null;
+//  }
+//
+//  public static void fillBoard(char[][] field) {
+//    for (int i = 0; i < field.length; i++) {
+//      for (int j = 0; j < field.length; j++) {
+//        field[i][j] = 0;
+//      }
+//    }
+//    return;
+//  }
 }
 
 
