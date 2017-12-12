ba42fafff5db1aa84187a9a840769ee979b166db
Lev Tesla <levonik99@gmail.com>
2017-12-12 23:39:36 +0200

    Homework 13 travisGone 2

---
+++ task_14/lev.brekhov/src/main/java/com/lev/TicTacToe.java
@@ -0,0 +1,28 @@
+package com.lev;
+
+public class TicTacToe {
+
+  public static void main(String[] args) {
+
+    char[][] field = new char[3][3];
+
+    char currentPlayer = 'X';
+    fillBoard(field);
+
+    while (!gameFinished()) {
+      String coords;
+      do {
+        coords = inputCoordinates();
+      } while (!validateCoordinates(coords));
+      makeMove(field, coords, currentPlayer);
+      currentPlayer = switchCurrentPlayer(currentPlayer);
+    }
+
+    calculateWinner();
+  }
+
+  public static void fillBoard(char[][] field) {
+  }
+}
+
+
