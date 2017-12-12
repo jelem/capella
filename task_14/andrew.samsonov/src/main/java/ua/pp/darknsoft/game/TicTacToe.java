c17c0209ef9393148e1559b95357fa09bcc8f3d0
darknsoft <samsonov.omr@gmail.com>
2017-12-12 21:44:47 +0200

    Home work 14 patch 01- JavaFX - with GUI with artifact TicTacToe.jar

--- task_14/andrew.samsonov/src/main/java/ua/pp/darknsoft/game/TicTacToe.java
+++ task_14/andrew.samsonov/src/main/java/ua/pp/darknsoft/game/TicTacToe.java
@@ -7,36 +7,28 @@ public class TicTacToe {
   private static int scoreX = 0;
   private static int scoreO = 0;
 
-  public static char[][] getField() {
-    return field;
-  }
-
-  public static void setField(char[][] field) {
-    TicTacToe.field = field;
-  }
-
   public static char getCurrentPlayer() {
-    return currentPlayer;
+    return TicTacToe.currentPlayer;
   }
 
   public static int getScoreX() {
-    return scoreX;
+    return TicTacToe.scoreX;
   }
 
   public static int getScoreO() {
-    return scoreO;
+    return TicTacToe.scoreO;
   }
 
   public static void setScore() {
     if (getCurrentPlayer() == 'X') {
-      scoreX++;
+      TicTacToe.scoreX++;
     } else {
-      scoreO++;
+      TicTacToe.scoreO++;
     }
   }
 
-  public static void setCurrentPlayer(char currentPlayer) {
-    TicTacToe.currentPlayer = currentPlayer;
+  public static void setCurrentPlayer(char curPlayer) {
+    TicTacToe.currentPlayer = curPlayer;
   }
 
   public static void switchCurrentPlayer(char curPlayer) {
@@ -48,11 +40,11 @@ public class TicTacToe {
   }
 
   public static void makeMove(int[] coords, char currentPlayer) {
-    field[coords[0]][coords[1]] = currentPlayer;
+    TicTacToe.field[coords[0]][coords[1]] = currentPlayer;
   }
 
   public static boolean validateCoordinates(int[] coords) {
-    if (field[coords[0]][coords[1]] == '\u0000') {
+    if (TicTacToe.field[coords[0]][coords[1]] == '\u0000') {
       return true;
     } else {
       return false;
@@ -68,26 +60,26 @@ public class TicTacToe {
 
     for (int i = 0; i <= 2; i++) {
       for (int j = 0; j <= 2; j++) {
-        if (j > 0 && getField()[i][j] != '\u0000' && getField()[i][j] == getField()[i][j - 1]) {
+        if (j > 0 && field[i][j] != '\u0000' && field[i][j] == field[i][j - 1]) {
           horizon++;
         }
         if (horizon == 2) {
           return true;
         }
-        if (i > 0 && getField()[i][j] != '\u0000' && getField()[i][j] == getField()[i - 1][j]) {
+        if (i > 0 && field[i][j] != '\u0000' && field[i][j] == field[i - 1][j]) {
           vertical++;
         }
         if (vertical == 3) {
           return true;
         }
-        if (i > 0 && i == j && getField()[i][j] != '\u0000' && getField()[i][j] == getField()[i
+        if (i > 0 && i == j && field[i][j] != '\u0000' && field[i][j] == field[i
             - 1][j - 1]) {
           leftDiagonal++;
         }
         if (leftDiagonal == 2) {
           return true;
         }
-        if (i > 0 && j < 2 && getField()[i][j] != '\u0000' && getField()[i][j] == getField()[i
+        if (i > 0 && j < 2 && field[i][j] != '\u0000' && field[i][j] == field[i
             - 1][j + 1]) {
           rightDiagonal++;
         }
@@ -107,10 +99,10 @@ public class TicTacToe {
         {'\u0000', '\u0000', '\u0000'},
         {'\u0000', '\u0000', '\u0000'}
     };
-    setField(chars);
+    TicTacToe.field = chars;
   }
 
-  private static void fillBoard(char[][] field) {
-    setField(field);
+  private static void fillBoard(char[][] otherField) {
+    TicTacToe.field = otherField;
   }
 }
