5749884bca05e1e81ee8c5e66219aca4bfee81dc
darknsoft <samsonov.omr@gmail.com>
2017-12-15 22:29:40 +0200

    Home work 15 Style - patch06

--- task_15/andrew.samsonov/src/main/java/ua/pp/darknsoft/enterprise/Main.java
+++ task_15/andrew.samsonov/src/main/java/ua/pp/darknsoft/enterprise/Main.java
@@ -7,13 +7,13 @@ import ua.pp.darknsoft.enterprise.heirs.Loader;
 public class Main {
 
   public static void main(String[] args) {
-    Worker boss = new Foreman("Евгениевич", "Технадзор");
-    Worker workerOne = new Digger("Петро", "Землекоп", Tools.ЛОПАТА);
-    Worker workerTwo = new Digger("Иван", "Землекоп", Tools.КИРКА);
-    Worker workerThree = new Loader("Степан", "Грузчик", Tools.ТЕЛЕЖКА);
-    Worker workerFour = new Loader("Эй ты", "Разнорабочий");
+    final Worker boss = new Foreman("Евгениевич", "Технадзор");
+    final Worker workerOne = new Digger("Петро", "Землекоп", Tools.ЛОПАТА);
+    final Worker workerTwo = new Digger("Иван", "Землекоп", Tools.КИРКА);
+    final Worker workerThree = new Loader("Степан", "Грузчик", Tools.ТЕЛЕЖКА);
+    final Worker workerFour = new Loader("Эй ты", "Разнорабочий");
 
-    final Worker[] company = new Worker[5];
+    Worker[] company = new Worker[5];
     company[0] = boss;
     company[1] = workerOne;
     company[2] = workerTwo;
