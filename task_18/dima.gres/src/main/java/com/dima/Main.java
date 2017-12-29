c325d88e1ccc2b9c84039c885614f2e248ebb1af
dima <greshchook@gmail.com>
2017-12-29 12:38:53 +0200

    zoo

--- task_18/dima.gres/src/main/java/com/dima/Main.java
+++ task_18/dima.gres/src/main/java/com/dima/Main.java
@@ -3,6 +3,10 @@ package com.dima;
 public class Main {
 
   public static void main(String[] args) {
-
+    MyList zoo = new MyList();
+    zoo.add("Monkey");
+    zoo.add("Dolphin");
+    zoo.add("Eagle");
+    zoo.add("Elephant");
   }
 }
