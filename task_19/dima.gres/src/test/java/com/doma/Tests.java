9da4cf0c0a72da83560b33ffe55a7387a98bcd6e
dima <greshchook@gmail.com>
2018-01-09 00:16:00 +0200

    looptest

--- task_19/dima.gres/src/test/java/com/doma/Tests.java
+++ task_19/dima.gres/src/test/java/com/doma/Tests.java
@@ -63,4 +63,19 @@ public class Tests {
     actual = list.get(2);
     assertEquals(actual, 3);
   }
+
+  @Test
+  public void getLoop() {
+    MyLinkedList list = new MyLinkedList();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    list.introduceCycle(2);
+    for (int i = 0; i < 6; i++) {
+      System.out.print(list.poll() + " ");
+    }
+    System.out.println();
+  }
 }
