96ba9965244b97214f79519d373b524543f93591
dima <greshchook@gmail.com>
2018-01-08 22:08:47 +0200

    dev linkedList

--- task_19/dima.gres/src/main/java/com/dima/CustomeLinkedList.java
+++ task_19/dima.gres/src/main/java/com/dima/CustomeLinkedList.java
@@ -1,6 +1,20 @@
 package com.dima;
 
+import java.util.Iterator;
+
 public interface CustomeLinkedList {
 
   void add(int element);
+
+  int poll();
+
+  int peek();
+
+  int get(int index);
+
+  void introduceCycle(int index);
+
+  boolean hasCycle();
+
+  Iterator ahead();
 }
--- task_19/dima.gres/src/main/java/com/dima/MyLinkedList.java
+++ task_19/dima.gres/src/main/java/com/dima/MyLinkedList.java
@@ -1,4 +1,40 @@
 package com.dima;
 
-public class MyLinkedList {
+import java.util.Iterator;
+
+public class MyLinkedList implements CustomeLinkedList {
+  @Override
+  public void add(int element) {
+
+  }
+
+  @Override
+  public int poll() {
+    return 0;
+  }
+
+  @Override
+  public int peek() {
+    return 0;
+  }
+
+  @Override
+  public int get(int index) {
+    return 0;
+  }
+
+  @Override
+  public void introduceCycle(int index) {
+
+  }
+
+  @Override
+  public boolean hasCycle() {
+    return false;
+  }
+
+  @Override
+  public Iterator ahead() {
+    return null;
+  }
 }
