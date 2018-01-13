86c15a36b0b741ae19dfec08000a8ed40802242e
dima <greshchook@gmail.com>
2018-01-14 00:30:26 +0200

    no-error

--- task_20/dima.gres/src/main/java/com/dima/City.java
+++ task_20/dima.gres/src/main/java/com/dima/City.java
@@ -18,6 +18,11 @@ public class City {
   }
 
   @Override
+  public int hashCode() {
+    return super.hashCode();
+  }
+
+  @Override
   public boolean equals(Object obj) {
     if (obj == null || this.getClass() != obj.getClass()) {
       return false;
