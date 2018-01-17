afa62a13dda44ea68d13306e751492f37eb5f5b0
dima <greshchook@gmail.com>
2018-01-17 11:44:17 +0200

    succesfull

--- task_20/dima.gres/src/main/java/com/dima/District.java
+++ task_20/dima.gres/src/main/java/com/dima/District.java
@@ -1,8 +1,9 @@
 package com.dima;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
-public class District implements Comparator<Student> {
+public class District implements Comparator<Student>, Serializable {
   @Override
   public int compare(Student tmp, Student tmp1) {
     if (tmp.getCity().getDistrict().compareTo(tmp1.getCity().getDistrict()) < 0) {
