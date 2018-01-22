60fb3576ee38958e02cc2eddb49fa63691687eed
dima <greshchook@gmail.com>
2018-01-22 14:53:16 +0200

    Builder person

--- task_22/dima.gres/src/main/java/Person.java
+++ task_22/dima.gres/src/main/java/Person.java
@@ -1,2 +1,51 @@
 public class Person {
+  private final static int Alpha = 'A';
+  private String name;
+  private String surname;
+  private int age;
+
+  private Person(Builder builder) {
+    this.name = builder.firstName;
+    this.surname = builder.secondName;
+    this.age = builder.old;
+  }
+
+  public static class Builder {
+    private String firstName;
+    private String secondName;
+    private int old;
+
+    public Builder name(String name) {
+      this.firstName = name;
+      return this;
+    }
+
+    public Builder surname(String surname) {
+      this.secondName = surname;
+      return this;
+    }
+
+    public Builder age(int old) {
+      this.old = old;
+      return this;
+    }
+
+    public Person build() {
+      return new Person(this);
+    }
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+
 }
