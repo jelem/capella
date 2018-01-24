fceef4d98658e89120a7c8c1f9e5ef20e5031638
dima <greshchook@gmail.com>
2018-01-24 12:54:28 +0200

    12:54

--- task_23/dima.gres/src/main/java/Express.java
+++ task_23/dima.gres/src/main/java/Express.java
@@ -2,12 +2,18 @@ public class Express {
   public static final int VAGONSPLACES = 36;
 
   private int quantiyOfVagones;
-  private int quantityOfTickets;
+  private int quantityOfFreeTickets;
   private Ticket[][] tickets;
+  private String fromTo;
 
-  public Express(int quontiyOfVagones) throws NoNummerOfPlace {
+  public String getFromTo() {
+    return fromTo;
+  }
+
+  public Express(int quontiyOfVagones, String fromTo) throws NoNummerOfPlace {
+    this.fromTo = fromTo;
     this.quantiyOfVagones = quontiyOfVagones;
-    quantityOfTickets = this.quantiyOfVagones * VAGONSPLACES;
+    quantityOfFreeTickets = this.quantiyOfVagones * VAGONSPLACES;
     tickets = new Ticket[quantiyOfVagones][VAGONSPLACES];
 
     for (int i = 0; i < quantiyOfVagones; i++) {
@@ -17,5 +23,35 @@ public class Express {
     }
   }
 
+  public int getQuantiyOfVagones() {
+    return quantiyOfVagones;
+  }
 
+  public int getQuantityOfTickets() {
+    return quantityOfFreeTickets;
+  }
+
+  public boolean isFull() {
+    return quantityOfFreeTickets == 0;
+  }
+
+  public Ticket getTickets(int vagon, int place) throws NoNummerOfPlace {
+    if (vagon < 0 || vagon >= quantiyOfVagones || place < 0 || place >= VAGONSPLACES) {
+      throw new NoNummerOfPlace("not valid nummer of vagon or/and nummer of place");
+    }
+
+    return tickets[vagon][place];
+  }
+
+  public void setTickets(int vagon, int place, Pasenger pasenger) throws NoNummerOfPlace, NoFreePlace {
+    if (vagon < 0 || vagon >= quantiyOfVagones || place < 0 || place >= VAGONSPLACES) {
+      throw new NoNummerOfPlace("not valid nummer of vagon or/and nummer of place");
+    }
+
+    tickets[vagon][place].set(pasenger);
+  }
+
+  public void setFromTo(String fromTo) {
+    this.fromTo = fromTo;
+  }
 }
