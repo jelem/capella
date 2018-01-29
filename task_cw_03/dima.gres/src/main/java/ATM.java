e5d04422b091d9720119218f3c823a07377b0884
dima <greshchook@gmail.com>
2018-01-29 17:48:20 +0200

    17:48

--- task_cw_03/dima.gres/src/main/java/ATM.java
+++ task_cw_03/dima.gres/src/main/java/ATM.java
@@ -1,2 +1,14 @@
 public class ATM {
+
+  private int maxOfTrans;
+  
+
+  public boolean checCard(Card card) {
+    return false;
+  }
+
+  public boolean checkPin(Card card) {
+    return  false;
+  }
+
 }
--- task_cw_03/dima.gres/src/main/java/Person.java
+++ task_cw_03/dima.gres/src/main/java/Person.java
@@ -1,2 +1,15 @@
 public class Person {
+  private int codeOfTrans;
+
+  public void putCardIntoATM(ATM atm) {
+
+  }
+
+  public void changeTransAct() {
+
+  }
+
+  public int getCodeOfTransTrans() {
+    return codeOfTrans;
+  }
 }
