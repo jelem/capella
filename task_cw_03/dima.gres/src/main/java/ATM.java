cdfb60bcfafeddc65d12ec52a9ed9e872c66b103
dima <greshchook@gmail.com>
2018-01-31 15:35:25 +0200

    begin

--- task_cw_03/dima.gres/src/main/java/ATM.java
+++ task_cw_03/dima.gres/src/main/java/ATM.java
@@ -1,14 +1,3 @@
 public class ATM {
 
-  private int maxOfTrans;
-  
-
-  public boolean checCard(Card card) {
-    return false;
-  }
-
-  public boolean checkPin(Card card) {
-    return  false;
-  }
-
 }
