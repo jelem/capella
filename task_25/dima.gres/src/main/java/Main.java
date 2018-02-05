027b187403ce87cb85354c0af4bdb90c4fcb936b
dima <greshchook@gmail.com>
2018-02-05 19:57:03 +0200

    19:56

--- task_25/dima.gres/src/main/java/Main.java
+++ task_25/dima.gres/src/main/java/Main.java
@@ -1,4 +1,8 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 public class Main {
 
