856a3175bc51c1e9a73051835e5393b555c7179d
dima <greshchook@gmail.com>
2018-02-12 22:38:03 +0200

    22:37

--- task_28/dima.gres/src/main/java/Country.java
+++ task_28/dima.gres/src/main/java/Country.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Country {
   private String name;
 
@@ -16,5 +18,34 @@ public class Country {
   public Country(String name) {
 
     this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "Country{"
+        + "name='"
+        + name
+        + '\''
+        + '}';
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    if (this == object) {
+      return true;
+    }
+
+    if (!(object instanceof Country)) {
+      return false;
+    }
+    
+    Country country = (Country) object;
+    return Objects.equals(getName(), country.getName());
+  }
+
+  @Override
+  public int hashCode() {
+
+    return Objects.hash(getName());
   }
 }
