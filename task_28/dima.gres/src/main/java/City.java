d182d18175e5071d2ff17c6d6e5a0ae76693e1e1
dima <greshchook@gmail.com>
2018-02-12 22:45:22 +0200

    22:45

--- task_28/dima.gres/src/main/java/City.java
+++ task_28/dima.gres/src/main/java/City.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class City {
   private String name;
   private Country country;
@@ -5,6 +7,39 @@ public class City {
   public City(String name, Country country) {
     this.name = name;
     this.country = country;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    if (this == object) {
+      return true;
+    }
+
+    if (!(object instanceof City)) {
+      return false;
+    }
+
+    City city = (City) object;
+    return Objects.equals(getName(), city.getName())
+        && Objects.equals(getCountry(), city.getCountry());
+  }
+
+  @Override
+  public int hashCode() {
+
+    return Objects.hash(getName(), getCountry());
+  }
+
+  @Override
+  public String toString() {
+    return "City{"
+        + "name='"
+        + name
+        + '\''
+        + ", country="
+        + country
+        + '}'
+        ;
   }
 
   public City() {
