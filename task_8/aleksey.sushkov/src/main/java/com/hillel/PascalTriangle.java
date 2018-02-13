948ddd05a0b6456cba5f5bc5d8b314ef0bfee3e7
Allekseys <33160295+Allekseys@users.noreply.github.com>
2018-02-13 21:27:27 +0200

    Update PascalTriangle.java

--- task_8/aleksey.sushkov/src/main/java/com/hillel/PascalTriangle.java
+++ task_8/aleksey.sushkov/src/main/java/com/hillel/PascalTriangle.java
@@ -21,14 +21,13 @@ public class PascalTriangle {
     arr[1][1] = 1;
     System.out.println(arr[0][0]);
     System.out.println(arr[1][0] + " " + arr[1][1]);
-    for (int i = 2; i < rows; i++)
-    {
-      arr[i] = new int [i+1];
+    for (int i = 2; i < rows; i++) {
+      arr[i] = new int [i + 1];
       arr[i][0] = 1;
       System.out.print(arr[i][0] + " ");
       for (int j = 1; j < i; j++)
       {
-        arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+        arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
         System.out.print(arr[i][j] + " ");
       }
       arr[i][i] = 1;
