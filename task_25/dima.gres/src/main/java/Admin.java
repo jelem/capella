552bb05504daf8ea78584ba35f5e8ee73850185c
dima <greshchook@gmail.com>
2018-02-16 14:45:42 +0200

    14:45

--- task_25/dima.gres/src/main/java/Admin.java
+++ task_25/dima.gres/src/main/java/Admin.java
@@ -3,6 +3,11 @@ import java.util.HashMap;
 public class Admin {
   HashMap<Author, Integer> authors;
 
+  private static final String SELECTAUTHORS = "select a.id, a.name, a.age\n"
+      + "from authors a\n"
+      + ";"
+      ;
+
   public Admin() {
     this.authors  = new HashMap<>();
   }
