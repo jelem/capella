4d84d70046c2fa83a787939ff568e07119c57476
dima <greshchook@gmail.com>
2018-02-16 11:27:52 +0200

    11:27

--- task_25/dima.gres/src/main/java/Main.java
+++ task_25/dima.gres/src/main/java/Main.java
@@ -1,5 +1,6 @@
 
 
+import java.io.IOException;
 import java.sql.Connection;
 
 import java.sql.DriverManager;
@@ -10,9 +11,27 @@ import java.sql.SQLException;
 
 import java.sql.Statement;
 
+import java.util.Properties;
+
 public class Main {
 
+  private static final String locPropertie = "/information.properties";
+
   public static void main(String[] args) {
+
+
+
+    try {
+      Properties properties = new Properties();
+      properties.load(Main.class.getResourceAsStream(locPropertie));
+
+    } catch (IOException exc) {
+
+      exc.printStackTrace();
+
+    }
+
+
 
   }
 
