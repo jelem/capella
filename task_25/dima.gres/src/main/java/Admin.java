438041efe2cee28b3c9a19444c47534e40a4fe21
dima <greshchook@gmail.com>
2018-02-16 14:36:26 +0200

    14:36

--- task_25/dima.gres/src/main/java/Admin.java
+++ task_25/dima.gres/src/main/java/Admin.java
@@ -1,3 +1,13 @@
-public class Admin {
+import java.util.HashMap;
 
+public class Admin {
+  HashMap<Author, Integer> authors;
+
+  public Admin() {
+    this.authors  = new HashMap<>();
+  }
+
+  public HashMap<Author, Integer> getAuthors() {
+    return authors;
+  }
 }
