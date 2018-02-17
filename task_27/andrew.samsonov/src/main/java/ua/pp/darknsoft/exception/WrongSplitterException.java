a88b3a949ba1c853835d50577aad3fda9555c7f3
darknsoft <samsonov.omr@gmail.com>
2018-02-17 02:30:09 +0200

    Home works 27 Patch 01

--- task_27/andrew.samsonov/src/main/java/ua/pp/darknsoft/exception/WrongSplitterException.java
+++ task_27/andrew.samsonov/src/main/java/ua/pp/darknsoft/exception/WrongSplitterException.java
@@ -1,8 +1,5 @@
 package ua.pp.darknsoft.exception;
 
-/**
- * Created by Andrew on 17.02.2018.
- */
 public class WrongSplitterException extends RuntimeException {
 
   public WrongSplitterException(String string) {
